1875020a7db6ccf996192e6ad81a08c5e9449ab4
arturoalviar <arturoalviar@gmail.com>
2015-11-07 00:35:16 -0800

    configured gulpfile to compile sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 'use strict';
 
 var gulp      = require('gulp');
+var scss      = require('gulp-sass');
+var cssMin    = require('gulp-minify-css');
 
 
-gulp.task('default');
+gulp.task('sassy', function(){
+  return gulp.src('./scss/**/*.scss')
+    .pipe(scss())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('minify', ['sassy'] , function(){
+  return gulp.src('./public/css/main.css')
+    .pipe(cssMin())
+    .pipe(gulp.dest('./public/css'));
+});
+
+
+gulp.task('build', ['minify']);
+
+gulp.task('default', ['build']);
