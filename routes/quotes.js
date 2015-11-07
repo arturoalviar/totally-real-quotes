81addb623bf864bcd0207eb3f7ba97b68e8d3cce
arturoalviar <arturoalviar@gmail.com>
2015-11-07 00:10:25 -0800

    Added quotes route and views. Fixed layout issues

---
+++ routes/quotes.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var names = require('../data/people.json');
+var quotes = require('../data/quotes.json');
+//get the data.js file
+var d = require('../helper/data.js');
+//create Data object which will store a list of quotes and people
+var Data = new d(names, quotes);
+
+//url consists of a name and quote id
+router.get('/:name/:id', function(req, res, next){
+  //store url :name and :id param
+  var name = req.params.name;
+  var id = req.params.id;
+  //name finds name in people.json
+  //id finds id in quotes.json
+  //rand id is used to generate new url
+  //rand name does the same as rand id
+  res.render('quotes', {
+    title: 'Totally Legit Data',
+    name: Data.names[name],
+    id: Data.quotes[id],
+    rand_id: Data.randomId(quotes),
+    rand_name: Data.randName(Data.nameList)
+  });
+});
+
+
+module.exports = router;
