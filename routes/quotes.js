52660c1b6024fb8bdafdc0cc91ed2fc16dae3147
arturoalviar <arturoalviar@gmail.com>
2015-11-07 00:19:32 -0800

    added error handling for bad urls

--- routes/quotes.js
+++ routes/quotes.js
@@ -12,17 +12,24 @@ router.get('/:name/:id', function(req, res, next){
   //store url :name and :id param
   var name = req.params.name;
   var id = req.params.id;
+
+  var tempId = parseInt(id, 10);
+  if( tempId < 0 || tempId > Data.quoteList.length || isNaN(id)){
+    res.render('error', {title: 'Totally Legit Quotes'});
+  }
+  else{
   //name finds name in people.json
   //id finds id in quotes.json
   //rand id is used to generate new url
   //rand name does the same as rand id
-  res.render('quotes', {
-    title: 'Totally Legit Data',
-    name: Data.names[name],
-    id: Data.quotes[id],
-    rand_id: Data.randomId(quotes),
-    rand_name: Data.randName(Data.nameList)
-  });
+    res.render('quotes', {
+      title: 'Totally Legit Data',
+      name: Data.names[name],
+      id: Data.quotes[id],
+      rand_id: Data.randomId(quotes),
+      rand_name: Data.randName(Data.nameList)
+    });
+  }
 });
 
 
