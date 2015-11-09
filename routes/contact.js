25ac992f5c247598ddad11599f855215a1e9b954
arturoalviar <arturoalviar@gmail.com>
2015-11-09 11:09:12 -0800

    changed title object string

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-  res.render('contact', { title: 'Totally Legit Quotes' });
+  res.render('contact', { title: 'Totally Real Quotes' });
 });
 
 
