007f975e4f3e057e315074aa8c1a3ebdc4fb1822
arturoalviar <arturoalviar@gmail.com>
2015-11-14 23:17:45 -0800

    improved router param validation, added comments

--- routes/quotes.js
+++ routes/quotes.js
@@ -5,6 +5,7 @@ var quotes = require('../data/quotes.json');
 //get the data.js file
 var d = require('../helper/data.js');
 //create Data object which will store a list of quotes and people
+//this acts as our "database"
 var Data = new d(names, quotes);
 
 //url consists of a name and quote id
@@ -13,19 +14,25 @@ router.get('/:name/:id', function(req, res, next){
   var name = req.params.name;
   var id = req.params.id;
 
-  var tempId = parseInt(id, 10);
-  if( tempId < 0 || tempId > Data.quoteList.length || isNaN(id)){
+  //look up :name and :id in data and store
+  var nameVal = Data.names[name];
+  var idVal = Data.quotes[id];
+
+  //validate that :name and :id exist in "database"; if undefined, it does not exist. Show error page
+  if(typeof(nameVal) === "undefined" || typeof(idVal) === "undefined" ){
     res.render('error', {title: 'Totally Real Quotes'});
   }
   else{
-  //name finds name in people.json
-  //id finds id in quotes.json
-  //rand id is used to generate new url
-  //rand name does the same as rand id
+  /*
+    1.name finds name in people.json
+    2.id finds id in quotes.json
+    3.rand id is used to generate new url
+    4.rand name does the same as rand id
+  */
     res.render('quotes', {
       title: 'Totally Real Quotes',
-      name: Data.names[name],
-      id: Data.quotes[id],
+      name: nameVal,
+      id: idVal,
       rand_id: Data.randomId(quotes),
       rand_name: Data.randName(Data.nameList)
     });
